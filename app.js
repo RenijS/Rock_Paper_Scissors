82607fcc176063b1eb295e1a1351547f87b2b7c3
RenijS <renijshrestha8@gmail.com>
2022-01-15 16:56:01 +1100

    Initial

---
+++ app.js
@@ -0,0 +1,54 @@
+function compPlay() {
+  const rand = Math.floor(Math.random() * 3) + 1;
+  console.log();
+  return rand;
+}
+function playerPlay() {
+  const choice = prompt("Choose your option");
+  if (choice === "1" || choice.toLowerCase() === "rock") {
+    return 1;
+  } else if (choice === "2" || choice.toLowerCase() === "paper") {
+    return 2;
+  } else if (choice === "3" || choice.toLowerCase() === "scissor") {
+    return 3;
+  }
+}
+
+function playGame(compChoice, playerChoice) {
+  if (compChoice === playerChoice) {
+    return "D";
+  } else if (compChoice === 3) {
+    if (playerChoice === 2) {
+      return "C";
+    }
+  } else if (compChoice === 2) {
+    if (playerChoice === 1) {
+      return "C";
+    }
+  } else if (compChoice === 1) {
+    if (playerChoice === 3) {
+      return "C";
+    }
+  }
+  return "P";
+}
+
+function game() {
+  let computer = 0;
+  let player = 0;
+  for (let i = 0; i < 5; i++) {
+    const game = playGame(compPlay(), playerPlay());
+    if (game === "C") {
+      computer++;
+    } else if (game === "P") {
+      player++;
+    }
+  }
+  if (computer > player) {
+    return `Computer won! with score ${computer}`;
+  } else if (player > computer) {
+    return `You won! with score ${player}`;
+  }
+}
+
+console.log(game());
